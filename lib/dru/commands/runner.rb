d2643f2b3f2ffdb07cd4edab14c56cfc743f4adb
Christian Rimondi <christian.rimondi@gmail.com>
2019-01-11 14:48:23 +0100

    Refactor runner command
    Extracted the initializer and helper methods. They will be used also in
    other commands.

--- lib/dru/commands/runner.rb
+++ lib/dru/commands/runner.rb
@@ -1,29 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../command'
+require_relative '../container_command'
 
 module Dru
   module Commands
-    class Runner < Dru::Command
-      def initialize(command, options)
-        raise MissingContainerError unless options[:container]
-
-        @options = options
-        @command = command || []
-      end
-
+    class Runner < Dru::ContainerCommand
       def execute(input: $stdin, output: $stdout)
         run_docker_compose_command('run', '--rm', '--entrypoint', 'sh -c', container, command, tty: true)
-      end
-
-      private
-
-      def command
-        @command.join(' ')
-      end
-
-      def container
-        options[:container]
       end
     end
   end
