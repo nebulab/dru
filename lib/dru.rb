87524d8b223632f79084168ce97ae3776a76e79d
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-01-25 16:22:48 +0100

    Move DOCKER_COMPOSE_COMMAND constant under Dru module
    It will be used in different places, so it shouldn't belong to
    Dru::Command.

--- lib/dru.rb
+++ lib/dru.rb
@@ -3,6 +3,7 @@ require "dru/cli"
 
 module Dru
   DRUCONFIG = File.expand_path('~/.druconfig') 
+  DOCKER_COMPOSE_COMMAND = 'docker-compose'.freeze
 
   def self.config
     Config.instance.tap { |instance| instance.config_file_path = DRUCONFIG }
