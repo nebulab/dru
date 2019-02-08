048618870402d244dbc5f9d090a58313322ed9ce
Christian Rimondi <christian.rimondi@gmail.com>
2019-02-08 16:23:30 +0100

    Prepend dru commands to ARGV
    Moving dru command at the beginning of the ARGV array allows Thor to
    parse the options also when used before the command. This is especially
    needed to keep consistency across docker-compose and dru custom
    commands.

--- lib/dru/argv.rb
+++ lib/dru/argv.rb
@@ -4,24 +4,50 @@ module Dru
   class Argv
     attr_reader :argv
 
+    def self.parse(*args)
+      new(*args).parse
+    end
+
     def initialize(argv = ARGV)
-      @argv = argv
+      @argv = argv.dup
     end
 
     def parse
-      return argv unless docker_compose_command_index
+      return argv unless known_command
 
-      argv.dup.insert(docker_compose_command_index, Thor::Options::OPTS_END)
+      if dru_command?(known_command)
+        parse_dru_command
+      else
+        parse_docker_compose_command
+      end
     end
 
     private
 
-    def docker_compose_command_index
-      argv.index { |arg| docker_compose_command?(arg) }
+    def parse_docker_compose_command
+      argv.insert(known_command_index, Thor::Options::OPTS_END)
     end
 
-    def docker_compose_command?(arg)
-      Dru::DOCKER_COMPOSE_COMMANDS.include?(arg)
+    def parse_dru_command
+      argv.unshift(argv.delete(known_command))
+    end
+
+    def known_command
+      argv[known_command_index] if known_command_index
+    end
+
+    def known_command_index
+      @known_command_index ||= argv.index do |arg|
+        dru_command?(arg) || docker_compose_command?(arg)
+      end
+    end
+
+    def docker_compose_command?(command)
+      Dru::DOCKER_COMPOSE_COMMANDS.include?(command)
+    end
+
+    def dru_command?(command)
+      Dru::CLI.commands.keys.push('help').include?(command)
     end
   end
 end
