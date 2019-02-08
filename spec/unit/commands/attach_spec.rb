93670bd0db0f5de9a2e16f2b52e0765713f78eba
Christian Rimondi <christian.rimondi@gmail.com>
2019-02-08 16:29:52 +0100

    Change attach command usage
    In this way, the command is more consistent with the docker-compose cli.

--- spec/unit/commands/attach_spec.rb
+++ spec/unit/commands/attach_spec.rb
@@ -1,32 +1,21 @@
 require 'dru/commands/attach'
 
 RSpec.describe Dru::Commands::Attach do
-  subject { Dru::Commands::Attach.new(options: options) }
+  subject { Dru::Commands::Attach.new(service: service, options: { detach_keys: detach_keys }) }
 
-  let(:container) { 'container' }
-  let(:options) { { container: container } }
+  let(:service) { 'service' }
+  let(:container_id) { 'container_id' }
+  let(:detach_keys) { 'ctrl-s' }
 
-  context 'when container is not set' do
-    let(:container) { nil }
-
-    it 'raises the MissingContainerError exception' do
-      expect { subject }.to raise_error(Dru::Command::MissingContainerError)
-    end
+  before do
+    allow(subject).to receive(:container_name_to_id).with(service).and_return(container_id)
   end
 
-  context 'when container is set' do
-    let(:container_id) { 'container_id' }
+  after do
+    subject.execute(output: StringIO.new)
+  end
 
-    before do
-      allow(subject).to receive(:container_name_to_id).and_return(container_id)
-    end
-
-    after do
-      subject.execute(output: StringIO.new)
-    end
-
-    it "executes docker attach on the container" do
-      expect(subject).to receive(:run_docker_command).with(*described_class::ATTACH_COMMAND, container_id)
-    end
+  it "executes docker attach on the container" do
+    expect(subject).to receive(:run_docker_command).with(described_class::ATTACH_COMMAND, "--detach-keys=#{detach_keys}", container_id)
   end
 end
