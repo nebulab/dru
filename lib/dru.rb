f84fe3ea4761468962e5c77cd37f9f25d1272202
Christian Rimondi <christian.rimondi@gmail.com>
2019-03-22 11:12:00 +0100

    Add `up` command to the docker-compose commands list

--- lib/dru.rb
+++ lib/dru.rb
@@ -6,9 +6,9 @@ module Dru
   DRUCONFIG = File.expand_path('~/.druconfig')
   DOCKER_COMMAND = 'docker'.freeze
   DOCKER_COMPOSE_COMMAND = 'docker-compose'.freeze
-  DOCKER_COMPOSE_COMMANDS = %w[build bundle config create events exec images kill
+  DOCKER_COMPOSE_COMMANDS = %w[build bundle config create down events exec images kill
                                logs pause port ps pull push restart rm run scale
-                               start stop top unpause down]
+                               start stop top unpause up]
 
   def self.config
     Config.instance.tap { |instance| instance.config_file_path = DRUCONFIG }
